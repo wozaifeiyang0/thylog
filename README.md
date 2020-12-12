598f3dcd01376f7632bf8fc86d5e271af65e65a6
Tang huai yu <tanghy@jianjukeji.com>
2020-12-12 15:53:55 +0800

    添加方法提供更改日志目录能力

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # thylog
+
+> 1 thy分为三种日志级别，分别是Info、Warning、Error
+>
+> 2 其中Error日志级别下，可生产日志文件
--- thylog.go
+++ thylog.go
@@ -22,3 +22,12 @@ func init() {
 	Warning = log.New(os.Stdout, "Warning:", log.Ldate|log.Ltime|log.Lshortfile)
 	Error = log.New(io.MultiWriter(os.Stderr, errFile), "Error:", log.Ldate|log.Ltime|log.Lshortfile)
 }
+
+/// 重新设计日志打印文件的路径
+func SetLogFile(path string, logFile string) {
+	errFile, err := os.OpenFile(path +"/"+ logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("打开日志文件失败：", err)
+	}
+	Error = log.New(io.MultiWriter(os.Stderr, errFile), "Error:", log.Ldate|log.Ltime|log.Lshortfile)
+}
